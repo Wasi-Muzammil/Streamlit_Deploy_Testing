228c02cce2c98dd9c062596210d990ae95c45cc1
Wasi-Muzammil <wasimuzammil2005@gmail.com>
2026-01-31 20:43:13 +0500

    Updated

--- main.py
+++ main.py
@@ -2,28 +2,14 @@
 
 # st.write("Fingerprint Attendance System")
 # st.write("Testing")
-
-import streamlit as st
-import psycopg2
-import os
 import sys
 import subprocess
-
-# For local testing only
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except:
-    pass
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 
 def check_dependencies():
     """Check if required packages are installed"""
     
     required_packages = [
-        'psycopg',
+        'psycopg2',
         'uvicorn',
         'streamlit',
         'pillow',
@@ -52,6 +38,21 @@ def check_dependencies():
     else:
         print("\n✅ All dependencies are installed")
 
+
+check_dependencies()
+import streamlit as st
+import psycopg2
+import os
+
+
+# For local testing only
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 
