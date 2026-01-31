17f75405ddb86aebf1cac15c60d7289d636bc6c6
Wasi-Muzammil <wasimuzammil2005@gmail.com>
2026-01-31 20:34:22 +0500

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-import streamlit as st
+# import streamlit as st
 
-st.write("Fingerprint Attendance System")
-st.write("Testing")
+# st.write("Fingerprint Attendance System")
+# st.write("Testing")
+
+import streamlit as st
+import psycopg2
+import os
+
+# For local testing only
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+st.title("Neon Database Test")
+
+if not DATABASE_URL:
+    st.error("DATABASE_URL not found")
+    st.stop()
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM user_information;")  
+    rows = cursor.fetchall()
+
+    st.success("Connected to Neon Database")
+
+    for row in rows:
+        st.write(row)
+
+    cursor.close()
+    conn.close()
+
+except Exception as e:
+    st.error("Database connection failed")
+    st.exception(e)
